5c68a88fb01498ffb589e904650f94980fb83e5f
dan7863 <danrico786@gmail.com>
2024-02-04 09:54:32 -0500

    Validaciones personalizadas por modelo

--- Models/TipoCuenta.cs
+++ Models/TipoCuenta.cs
@@ -1,12 +1,14 @@
 ﻿namespace ManejoPresupuesto;
+
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using ManejoPresupuesto.validaciones;
 
-public class TipoCuenta
+public class TipoCuenta //: IValidatableObject
 {
     public int Id { get; set; }
     [Required(ErrorMessage = "El campo {0} es requerido")]
-    [PrimeraLetraMayuscula]
+    //[PrimeraLetraMayuscula]
     // [StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage = "La longitud del campo {0} debe estar entre {2} y {1}")]
     // [Display(Name = "Nombre del tipo cuenta")]
     public string Nombre { get; set; }
@@ -24,4 +26,16 @@ public class TipoCuenta
     // [CreditCard(ErrorMessage = "La tarjeta de crédito no es válida")]
     // [Display(Name = "Tarjeta De Crédito")]
     public string TarjetaDeCredito { get; set; }
+
+    // public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    // {
+    //     if(Nombre != null && Nombre.Length > 0){
+    //         var primeraLetra = Nombre[0].ToString();
+
+    //         if(primeraLetra != primeraLetra.ToUpper()){
+    //             yield return new ValidationResult("La primera letra debe ser mayúscula", 
+    //             new [] { nameof(Nombre) });
+    //         }
+    //     }
+    // }
 }
