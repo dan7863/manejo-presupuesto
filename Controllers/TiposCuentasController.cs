4949ba55c66ef9eb8661455ed522bbc7ca11be48
dan7863 <danrico786@gmail.com>
2024-02-04 11:01:47 -0500

    Connection strings to db, dapper and microsoft sql client

--- Controllers/TiposCuentasController.cs
+++ Controllers/TiposCuentasController.cs
@@ -1,13 +1,30 @@
 ﻿namespace ManejoPresupuesto.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using ManejoPresupuesto.Models;
+using Microsoft.Data.SqlClient;
+using Dapper;
 
 public class TiposCuentasController : Controller
 {
-    public IActionResult Crear()
-    {
-        return View();
+    // private readonly string connectionString;
+    private readonly ILogger<HomeController> _logger;
+
+    public TiposCuentasController(// IConfiguration configuration, 
+    ILogger<HomeController> logger){
+        // connectionString = configuration.GetConnectionString("DefaultConnection");
+         _logger = logger;
     }
+
+    // public IActionResult Crear()
+    // {
+    //     using(var connection = new SqlConnection(connectionString))
+    //     {
+    //         var query = connection.Query("SELECT 1").FirstOrDefault();
+    //         Console.WriteLine(query);
+            
+    //     }
+    //     return View();
+    // }
 
     [HttpPost]
     public IActionResult Crear(TipoCuenta tipoCuenta){
