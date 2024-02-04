8da3bd031645848726ee291794582da8070b9515
dan7863 <danrico786@gmail.com>
2024-02-04 09:40:30 -0500

    Validaciones personalizadas

--- Models/TipoCuenta.cs
+++ Models/TipoCuenta.cs
@@ -1,10 +1,12 @@
 ﻿namespace ManejoPresupuesto;
 using System.ComponentModel.DataAnnotations;
+using ManejoPresupuesto.validaciones;
 
 public class TipoCuenta
 {
     public int Id { get; set; }
     [Required(ErrorMessage = "El campo {0} es requerido")]
+    [PrimeraLetraMayuscula]
     // [StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage = "La longitud del campo {0} debe estar entre {2} y {1}")]
     // [Display(Name = "Nombre del tipo cuenta")]
     public string Nombre { get; set; }
